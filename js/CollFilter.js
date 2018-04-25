b2ee8f3e0c2ae9dc1d9c9dbf9bc9e017debf8cae
zx <895252509@qq.com>
2018-04-25 13:58:40 +0800

    3

--- js/CollFilter.js
+++ js/CollFilter.js
@@ -1,19 +1,3 @@
-// var CollaborativeFilter = (function() {
-
-//     function pred(params) {
-
-//     }
-
-//     function sim_cal(params) {
-
-//     }
-
-//     function test() {
-
-//     }
-
-// })();
-
 function CollaborativeFilter() {
     this.movie_user = [];
     this.user_movie = [];
@@ -23,14 +7,19 @@ function CollaborativeFilter() {
 
     this.data = [
         [0, 0, 1],
-        [0, 1, 1],
-        [0, 2, 1],
-        [1, 0, 10],
-        [1, 1, 5],
-        [1, 2, 1],
+        [0, 1, 8],
+        [0, 2, 2],
+        [1, 0, 2],
+        [1, 1, 2],
+        [1, 2, 5],
         [2, 0, 1],
-        [2, 1, 2],
-        [2, 2, 10],
+        [2, 1, 9],
+        [2, 2, 1],
+        [3, 0, 3],
+        [3, 1, 9],
+        [3, 2, 5],
+        [4, 0, 3],
+        [4, 1, 6]
     ];
 
     for (var i = 0; i < this.data.length; i++) {
@@ -61,7 +50,7 @@ CollaborativeFilter.prototype = {
         if (this.similarity[m1][m2] != -1) return this.similarity[m1][m2];
 
         var si = [];
-
+        //找到同时给m1和m2都评过分的用户
         for (var user in this.movie_user[m1]) {
             if (user in this.movie_user[m2]) {
                 si[user] = 1;
@@ -74,24 +63,26 @@ CollaborativeFilter.prototype = {
             this.similarity[m2][m1] = 1;
             return 1;
         }
+
+        //分别把这些用户对这两个电影的评分做成数组
         var s1 = [];
         var s2 = [];
         for (var user in si) {
             s1.push(this.movie_user[m1][user]);
             s2.push(this.movie_user[m2][user]);
         }
-
+        //对这两个数组分别求和
         var sum1 = this.sum(s1);
-        var sum2 = this.sum(s1);
-
+        var sum2 = this.sum(s2);
+        //求数组值的平方的和
         var s1sq = this.sum(this.sqarr(s1));
         var s2sq = this.sum(this.sqarr(s2));
-
+        //
         var psum = this.sum(this.ama(s1, s2));
 
         var num = psum - (sum1 * sum2 / n);
 
-        var den = Math.sqrt((s1sq - sum1 * sum1 / n) * (s2sq - sum2 * sum2 / n));
+        var den = Math.sqrt((s1sq - Math.pow(sum1, 2) / n) * (s2sq - Math.pow(sum2, 2) / n));
 
         if (den == 0) {
             this.similarity[m1][m2] = 0;
@@ -107,18 +98,11 @@ CollaborativeFilter.prototype = {
         var sim_accumulate = 0.0;
         var rat_acc = 0.0;
 
-        /**
-         * user_movie
-         *     movie
-         * user  1    2    3    4
-         *   1   1    1    1    1
-         *   2   1    1    1    1
-         * 
-         */
+
         for (var item in this.user_movie[uid]) {
             // 计算该物品与待评价物品的相似度 
             var sim = this.sim_cal(item, mid);
-            if (sim < 0) continue;
+            //if (sim < 0) continue;
             //
             rat_acc += sim * this.user_movie[uid][item];
             // 相似度的和
@@ -131,15 +115,16 @@ CollaborativeFilter.prototype = {
         return rat_acc / sim_accumulate;
     },
     test: function() {
-        var output = [];
-        var sums = 0;
-        for (var i = 0; i < this.data.length; i++) {
-            var pre = this.pred(this.data[i][0], this.data[i][1]);
-            output.push(pre);
-            sums += (pre - this.data[i][2]) * (pre - this.data[i][2])
-        }
-        var rmse = Math.sqrt(sums / this.data.length);
+        // var output = [];
+        // var sums = 0;
+        // for (var i = 0; i < this.data.length; i++) {
+        //     var pre = this.pred(this.data[i][0], this.data[i][1]);
+        //     output.push(pre);
+        //     sums += (pre - this.data[i][2]) * (pre - this.data[i][2])
+        // }
+        // var rmse = Math.sqrt(sums / this.data.length);
 
+        var sim = this.sim_cal(0, 2);
     },
     sum: function(s) {
         var sm = 0;
@@ -149,10 +134,11 @@ CollaborativeFilter.prototype = {
         return sm;
     },
     sqarr: function(arr) {
+        var arr1 = [];
         for (var i = 0; i < arr.length; i++) {
-            arr[i] = arr[i] * arr[i];
+            arr1.push(Math.pow(arr[i], 2));
         }
-        return arr;
+        return arr1;
     },
     ama: function(a1, a2) {
         if (a1.length !== a2.length) return null;
